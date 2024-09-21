51b43b5ca7a11a12b1a0e33d5cb48e0b1be974aa
vishnu <vishnusreddy007@gmail.com>
2024-09-22 00:20:37 +0530

    Cache created in Application Class

--- app/src/main/java/com/androiddd/exovideoplayer/ExoVideoPlayer.kt
+++ app/src/main/java/com/androiddd/exovideoplayer/ExoVideoPlayer.kt
@@ -1,7 +1,34 @@
 package com.androiddd.exovideoplayer
 
 import android.app.Application
+import androidx.annotation.OptIn
+import androidx.media3.common.util.UnstableApi
+import androidx.media3.database.StandaloneDatabaseProvider
+import androidx.media3.datasource.cache.LeastRecentlyUsedCacheEvictor
+import androidx.media3.datasource.cache.SimpleCache
+import java.io.File
 
-class ExoVideoPlayer: Application() {
+class ExoVideoPlayer : Application() {
 
+    companion object {
+        lateinit var instance: ExoVideoPlayer
+
+        @get:UnstableApi
+        val simpleCache by lazy {
+            getSimpleCache()
+        }
+
+        @OptIn(UnstableApi::class)
+        private fun getSimpleCache(): SimpleCache {
+            val cacheSize = 100 * 1024 * 1024 // 100MB cache
+            val cacheEvictor = LeastRecentlyUsedCacheEvictor(cacheSize.toLong())
+            val databaseProvider = StandaloneDatabaseProvider(instance)
+            return SimpleCache(File(instance.cacheDir, "media"), cacheEvictor, databaseProvider)
+        }
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        instance = this
+    }
 }
